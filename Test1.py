9ce86cff214b543505433030ff62ff78b4d273d6
Don Thomas <40792806+don-ufo@users.noreply.github.com>
2018-08-09 15:12:05 +0530

    python test

---
+++ Test1.py
@@ -0,0 +1,16 @@
+print("Hello");
+a = 100
+b = 200
+print("Total:",a + b)
+
+text = "DON C THOMAS"
+print(text)
+print("First:",text[0:3])
+print("Last:",text[6:12])
+
+samplestring = "HELLO TEST"
+print(samplestring)
+newsample=samplestring.replace ('TEST' , 'DON')
+print(newsample)
+
+print(reversed(text))
